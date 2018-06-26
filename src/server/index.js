bb98561c481b81205eb638d3c6e4e552b9bd40b4
Kyle Coburn <kylecoburn@gmail.com>
2018-06-25 17:28:31 -0700

    ♻️ Include server env in subfolder

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv')
 
-dotenv.config({ path: '.env.server' })
-dotenv.config({ path: `.env.server.${process.env.NODE_ENV || 'development'}` })
+dotenv.config({ path: 'src/server/config/.env' })
+dotenv.config({ path: `src/server/config/.env.${process.env.NODE_ENV || 'development'}` })
 
 //EXPRESS
 
