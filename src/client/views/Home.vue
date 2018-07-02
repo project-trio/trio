fb3e788d00028d1d152ceea5b7cd2193504cb8e2
Kyle Coburn <kylecoburn@gmail.com>
2018-07-02 00:19:52 -0700

    💄 Show filter target header

--- src/client/views/Home.vue
+++ src/client/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
 <div class="home">
+	<div v-if="user" class="flex">
+		<Avatar :size="48" :ccid="userFilter.ccid" :md5="userFilter.md5" />
+		<h1>{{ userFilter.name }}</h1>
+	</div>
+	<div v-else-if="topic">
+		<h1>{{ topic }}</h1>
+		<p v-if="topicLink">Play now: <a :href="topicLink" target="_blank">{{ topicLink }}</a></p>
+	</div>
 	<CreateActivity />
 	<ul v-if="activities">
 		<li v-for="activity in activities" :key="activity.id">
@@ -12,10 +20,12 @@
 <script>
 import Activity from '@/client/components/Activity'
 import CreateActivity from '@/client/components/CreateActivity'
+import Avatar from '@/client/components/Avatar'
 
 export default {
 	components: {
 		Activity,
+		Avatar,
 		CreateActivity,
 	},
 
@@ -25,14 +35,17 @@ export default {
 	},
 
 	computed: {
-		userId () {
+		userFilter () {
 			const users = this.$store.state.users
 			for (const id in users) {
 				const user = users[id]
 				if (user.name === this.user) {
-					return parseInt(id, 10)
+					return user
 				}
 			}
+		},
+		userId () {
+			return this.userFilter.id
 		},
 		topicId () {
 			const topics = this.$store.state.topics
@@ -40,6 +53,14 @@ export default {
 				if (topics[id] === this.topic) {
 					return parseInt(id, 10)
 				}
+			}
+		},
+		topicLink () {
+			if (this.topic === 'TD') {
+				return 'https://td.suzu.online'
+			}
+			if (this.topic === 'Moba') {
+				return 'https://moba.suzu.online'
 			}
 		},
 
