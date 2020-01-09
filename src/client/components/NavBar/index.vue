2f1545c4dadd5ffa956cab71c726f804dc01d9ac
Kyle Coburn <kylecoburn@gmail.com>
2020-01-08 21:26:40 -0800

    🖼🌱 Brand icon

--- src/client/components/NavBar/index.vue
+++ src/client/components/NavBar/index.vue
@@ -1,13 +1,13 @@
 <template>
-<nav>
-	<div class="container  h-10 text-secondary  flex justify-between items-center">
-		<router-link :to="{ name: 'Home' }" class="text-xl font-black tracking-wide">Trio</router-link>
+<header>
+	<nav class="container  h-12 text-secondary  flex justify-between items-center">
+		<router-link :to="{ name: 'Home' }" class="text-xl font-black tracking-wide"><img src="icon.png" class="inline wh-8 pr-1">Trio</router-link>
 		<router-link v-if="name" :to="{ name: 'User', params: { name } }" class="flex items-center">
-			<span class="text-sm">{{ name }}</span>
-			<Avatar :size="24" :ccid="ccid" :md5="md5" class="ml-1" />
+			<span>{{ name }}</span>
+			<Avatar :size="26" :ccid="ccid" :md5="md5" class="ml-1" />
 		</router-link>
-	</div>
-</nav>
+	</nav>
+</header>
 </template>
 
 <script>
