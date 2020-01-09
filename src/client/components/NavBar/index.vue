9907faa1f540b7f18a6d66e7420700d574ae5071
Kyle Coburn <kylecoburn@gmail.com>
2020-01-08 22:14:27 -0800

    💄 Spin logo on hover

--- src/client/components/NavBar/index.vue
+++ src/client/components/NavBar/index.vue
@@ -1,7 +1,7 @@
 <template>
 <header>
 	<nav class="container  h-12 text-secondary  flex justify-between items-center">
-		<router-link :to="{ name: 'Home' }" class="text-xl font-black tracking-wide"><img src="icon.png" class="inline wh-8 pr-1">Trio</router-link>
+		<router-link :to="{ name: 'Home' }" class="logo-link"><img src="/icon.png" class="logo">Trio</router-link>
 		<router-link v-if="name" :to="{ name: 'User', params: { name } }" class="flex items-center">
 			<span>{{ name }}</span>
 			<Avatar :size="26" :ccid="ccid" :md5="md5" class="ml-1" />
@@ -33,3 +33,23 @@ export default {
 	},
 }
 </script>
+
+<style lang="postcss" scoped>
+.logo {
+	@apply inline wh-8 pr-1;
+	transition-property: transform;
+	transition-duration: 300ms;
+}
+
+.logo-link {
+	@apply text-xl font-black tracking-wide;
+	&:hover {
+		& .logo {
+			transform: rotate(30deg);
+		}
+		&:not(.router-link-exact-active):active .logo {
+			transform: rotate(90deg);
+		}
+	}
+}
+</style>
